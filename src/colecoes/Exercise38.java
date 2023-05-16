f26f168aaac7df04d792fba051d02b3f80e8f1cf
RenAlv08 <renan.c.alves03@gmail.com>
2023-05-16 16:26:53 -0300

    Exercício 38 de coleções resolvido

---
+++ src/colecoes/Exercise38.java
@@ -0,0 +1,23 @@
+package colecoes;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Exercise38 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+
+        Scanner scan = new Scanner(System.in);
+        String str;
+
+        do{
+            str = scan.nextLine();
+            if (!list.contains(str) && !str.equals("")) list.add(str);
+            else break;
+        }while(true);
+
+        for (int i = list.size() - 1; i >= 0; i--){
+            System.out.println(list.get(i));
+        }
+    }
+}
